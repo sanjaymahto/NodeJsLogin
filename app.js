25bbbc4d414f6886dcfc005fd7d90de4482cb2a1
sanjaymahto <mahto.sanjay41@gmail.com>
2017-06-22 13:03:18 +0530

    Login Using NodeJS

---
+++ app.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var cookieParser = require('cookie-parser');
+var MongoStore = require('connect-mongo')(session);
+
+var app = express();
+
+app.locals.pretty = true;
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'jade');
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+app.use(express.static(__dirname + '/app/public'));
+
+// build mongo database connection url //
+
+var dbHost = process.env.DB_HOST || 'localhost'
+var dbPort = process.env.DB_PORT || 27017;
+var dbName = process.env.DB_NAME || 'E-commerceDB';
+
+var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+if (app.get('env') == 'live'){
+// prepend url with authentication credentials // 
+	dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
+}
+// initialization of session middleware 
+//App level middleware
+app.use(session({
+  name :'myCustomCookie',
+  secret: 'myAppSecret', // encryption key 
+  resave: true,
+  httpOnly : true,
+  saveUninitialized: true,
+ store: new MongoStore({ url: dbURL }),
+  cookie: { secure: false }
+}));
+
+require('./app/server/routes')(app);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
